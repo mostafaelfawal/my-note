39727daef149b4627bda8c6bb9548569ecbd14ac
mostafaelfawal <armostafa982@gmail.com>
2025-11-26 04:18:25 +0200

    اضافة موديل الملاحظه و المستخدم 🙍‍♂️

---
+++ backend/models/note.model.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: String,
+    content: String,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Note", noteSchema);
