842f1f983b0b797a0fabe5742de00f130d5b4b91
mostafaelfawal <armostafa982@gmail.com>
2025-12-01 02:39:14 +0200

    return

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,8 @@
-// app.ts
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth.routes";
 import notesRouter from "./routes/notes.routes";
 import cookieParser from "cookie-parser";
-import serverless from "@vendia/serverless-express";
-import connectDB from "./config/db";
 
 const app = express();
 
@@ -17,37 +14,9 @@ app.use(
   })
 );
 app.use(cookieParser());
-declare global {
-  var mongoConnected: boolean | undefined;
-}
-app.use(async (req, res, next) => {
-  if (!global.mongoConnected) {
-    try {
-      await connectDB();
-      global.mongoConnected = true;
-    } catch (err) {
-      console.error("MongoDB connection error:", err);
-      return res.status(500).json({ message: "Database connection failed" });
-    }
-  }
-  next();
-});
 
-app.get("/", (req, res) => res.send("Hello world"));
 
 app.use("/api", authRouter);
 app.use("/api/notes", notesRouter);
 
-// هذا لـ Vercel
-export const handler = serverless({ app });
-
-// هذا للـ local development فقط
-if ("production" !== "production") {
-  import("dotenv").then((dotenv) => dotenv.config());
-  import("./config/db").then((module) => module.default());
-
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () =>
-    console.log(`Server running on http://localhost:${PORT}`)
-  );
-}
+export default app;
